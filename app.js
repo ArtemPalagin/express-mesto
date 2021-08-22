2c70781cfcd07de43623dd9075652c1c2204745c
ArtemPalagin <art.palagin@gmail.com>
2021-08-22 22:10:19 +0300

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
   useCreateIndex: true,
-    useFindAndModify: false
+  useFindAndModify: false,
+  useUnifiedTopology: true
 });
 app.use((req, res, next) => {
   req.user = {
