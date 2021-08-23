63ca6e982fdb5bf2b84464e8622bf5cdfe472c0a
ArtemPalagin <art.palagin@gmail.com>
2021-08-23 20:29:16 +0300

    Add files via upload

---
+++ routes/card.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+const {
+  getCards, postCard, deleteCard, likeCard, dislikeCard,
+} = require('../controllers/card');
+
+router.get('/cards', getCards);
+router.post('/cards', postCard);
+router.delete('/cards/:cardId', deleteCard);
+router.put('/cards/:cardId/likes', likeCard);
+router.delete('/cards/:cardId/likes', dislikeCard);
+
+module.exports = router;
