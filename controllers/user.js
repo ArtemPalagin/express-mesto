b018c6aa13793ccdfa20d7534cc80dea86f3a790
ArtemPalagin <art.palagin@gmail.com>
2021-08-23 18:43:21 +0300

    Update user.js

--- controllers/user.js
+++ controllers/user.js
@@ -2,33 +2,39 @@ const User = require('../models/user');
 
 module.exports.getUsers = (req, res) => {
   User.find({})
-  .then(users => res.send({ data: users }))
-  .catch(err => res.status(500).send({ message: err.message }));
+  .then((users) => {
+    return res.send({ data: users })
+  })
+  .catch((err) => {
+    return res.status(500).send({ message: err.message })
+  });
 }
 module.exports.getUser = (req, res) => {
   User.findById(req.params.userId)
   .then((user) => {
     if(!user){
-      res.status(404).send({ message: "Такого пользователя не существует" })
+      return res.status(404).send({ message: "Такого пользователя не существует" })
     }
-    res.send({ data: user })
+    return res.send({ data: user })
   })
   .catch((err) => {
     if (err.name === 'CastError') {
-      res.status(400).send({ message: 'Невалидный id ' });
+      return res.status(400).send({ message: 'Невалидный id ' });
     }
-    res.status(500).send({ message: err.message })
+    return res.status(500).send({ message: err.message })
   });
 }
 module.exports.postUser = (req, res) => {
   const { name, about, avatar } = req.body;
   User.create({ name, about, avatar })
-    .then(user => res.send({ data: user }))
+    .then((user) => {
+      return res.send({ data: user })
+    })
     .catch((err) => {
       if (err.name === 'ValidationError') {
-        res.status(400).send({ message: 'Некорректные данные' });
+        return res.status(400).send({ message: 'Некорректные данные' });
       }
-      res.status(500).send({ message: err.message })
+      return res.status(500).send({ message: err.message })
     });
 }
 module.exports.patchProfile = (req, res) => {
@@ -36,18 +42,18 @@ module.exports.patchProfile = (req, res) => {
   User.findByIdAndUpdate(req.user._id, { name, about }, { new: true, runValidators: true })
   .then((user) => {
     if(!user){
-      res.status(404).send({ message: "Такого пользователя не существует" })
+      return res.status(404).send({ message: "Такого пользователя не существует" })
     }
-    res.send({ data: user })
+    return res.send({ data: user })
   })
   .catch((err) => {
     if (err.name === 'ValidationError') {
-      res.status(400).send({ message: 'Некорректные данные' });
+      return res.status(400).send({ message: 'Некорректные данные' });
     }
     if (err.name === 'CastError') {
-      res.status(400).send({ message: 'Невалидный id ' });
+      return res.status(400).send({ message: 'Невалидный id ' });
     }
-    res.status(500).send({ message: err.message })
+    return res.status(500).send({ message: err.message })
   });
 }
 module.exports.patchAvatar = (req, res) => {
@@ -55,17 +61,17 @@ module.exports.patchAvatar = (req, res) => {
   User.findByIdAndUpdate(req.user._id, { avatar }, { new: true, runValidators: true })
   .then((user) => {
     if(!user){
-      res.status(404).send({ message: "Такого пользователя не существует" })
+      return res.status(404).send({ message: "Такого пользователя не существует" })
     }
-    res.send({ data: user })
+    return res.send({ data: user })
   })
   .catch((err) => {
     if (err.name === 'ValidationError') {
-      res.status(400).send({ message: 'Некорректные данные' });
+      return res.status(400).send({ message: 'Некорректные данные' });
     }
     if (err.name === 'CastError') {
-      res.status(400).send({ message: 'Невалидный id ' });
+      return res.status(400).send({ message: 'Невалидный id ' });
     }
-    res.status(500).send({ message: err.message })
+    return res.status(500).send({ message: err.message })
   });
 }
