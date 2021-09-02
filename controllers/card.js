025f39dd3adf99c76d482a75256a2446edfad251
ArtemPalagin <art.palagin@gmail.com>
2021-09-02 21:09:50 +0300

    исправил deleCard

--- controllers/card.js
+++ controllers/card.js
@@ -31,17 +31,18 @@ module.exports.deleteCard = (req, res, next) => {
       if (`${req.user._id}` !== `${card.owner}`) {
         throw new ForbiddenError('Чужую карточку удалять нельзя');
       }
+
+      return Card.findByIdAndRemove(req.params.cardId)
+        .then((removedCard) => res.send(removedCard))
+        .catch((err) => {
+          throw err;
+        });
     })
     .catch((err) => {
       if (err.name === 'CastError') {
         throw new RequestError('Невалидный id');
       }
     }).catch(next);
-
-  Card.findByIdAndRemove(req.params.cardId).then((card) => res.send(card)).catch((err) => {
-    throw err;
-  })
-    .catch(next);
 };
 module.exports.likeCard = (req, res, next) => {
   Card.findByIdAndUpdate(req.params.cardId, { $addToSet: { likes: req.user._id } }, { new: true })
